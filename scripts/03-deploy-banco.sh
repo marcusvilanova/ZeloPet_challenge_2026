ced90cad2873fcc2b461ffb022fd6bac85bc158a
bruno ferreira <brunoferr2014@gmail.com>
2026-09-12 00:59:27 +0000

    fix: compatibilizar MySQL com Azure Files

--- scripts/03-deploy-banco.sh
+++ scripts/03-deploy-banco.sh
@@ -5,8 +5,13 @@ require_secret MYSQL_ROOT_PASSWORD MYSQL_USER MYSQL_PASSWORD
 
 ACR_USERNAME=$(az keyvault secret show --vault-name "$KEY_VAULT" --name acr-username --query value -o tsv)
 ACR_PASSWORD=$(az keyvault secret show --vault-name "$KEY_VAULT" --name acr-password --query value -o tsv)
-STORAGE_KEY=$(az storage account keys list --resource-group "$RESOURCE_GROUP" \
-  --account-name "$STORAGE_ACCOUNT" --query '[0].value' -o tsv)
+SUBSCRIPTION_ID=$(az account show --query id --output tsv)
+
+STORAGE_KEY=$(az rest \
+  --method post \
+  --url "https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Storage/storageAccounts/${STORAGE_ACCOUNT}/listKeys?api-version=2024-01-01" \
+  --query 'keys[0].value' \
+  --output tsv)
 
 az container create --resource-group "$RESOURCE_GROUP" --name "$DB_ACI" \
   --image "$ACR_NAME.azurecr.io/$DB_IMAGE:$IMAGE_TAG" --cpu 1 --memory 2 \
